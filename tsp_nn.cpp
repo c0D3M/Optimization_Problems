dc0a7fb72528447f80c95a07f988904bacdf0abf
Chitrang Srivastava <chitrang.srivastava@gmail.com>
2019-02-25 15:56:47 +0530

    final version
    TSP solution passed for 1-5 with 10 points each.
    MST with double edge is used for tour construction.
    Used GLS/FLS with 2-Opt.

--- tsp_nn.cpp
+++ tsp_nn.cpp
@@ -2,11 +2,211 @@
 #include <algorithm>
 
 using namespace std;
-float **adjMatrix;
-vector<int> Tour;
+#ifdef DEBUG
+    #define LOG(x) std::cerr << x << std::endl
+#else
+    #define LOG(x)
+#endif
+
+int n; // Number of cities
+float **adjMatrix; // distance matrix
+vector<int> Tour; // Final tour
+vector<int> BestTour;
+float cost2 =0;
+float bestCost = 0.0; // best found so far
+
+// FLS/GLS
+int **penalty;
 bool *isActive;
+float alpha = 0.125;
+#define MAX_NO_IMPROVEMENT 100000
+bool logPrint=0;
+// Prototype
+float calculateAugCost(float cost);
 float getDistance (int a, int b);
 float getDelta(int a, int b, int c, int d);
+void updatePenalty();
+void clear();
+int FastIteration();
+float DeltaC, DeltaP;
+float getCost()
+{
+	float tcost =0.0;
+	for (int i=0; i<n; i++)
+		tcost += adjMatrix[Tour[i]][Tour[(i+1)%n]];
+	return tcost;
+}
+
+int next(int city, int jump=1)
+{
+	return (city+jump)%n;
+}
+int prev(int city, int jump=1)
+{
+	if(jump)
+		return (city-jump+n)%n;
+	else
+		return city;
+}
+int getLength(int start, int end)
+{
+	if(start > end) // loop around case i.e. (50, 0) 
+		return n + end-start;
+	else
+		return end-start;
+}
+void reverseTour(int start, int end)
+{
+	int count= abs(start-end);
+	if(start >end)
+		count = n -count+1;
+	else
+		count += 1;
+	count /= 2;
+	for (int i=0; i<count; i++)
+		std::swap(Tour[next(start, i)], Tour[prev(end, i)]);
+}
+// a, b, c, d  are indices in Tour array
+float getDistance (int a, int b)
+{
+	return adjMatrix[Tour[a]][Tour[b]];
+}
+// a, b, c, d  are indices in Tour array
+float getDelta(int a, int b, int c, int d)
+{
+	float ab =  getDistance(a, b);	
+	float ac =  getDistance(a, c);
+	float bd =  getDistance(b, d);
+	float cd =  getDistance(c, d);
+	if((ac > ab) && (bd > cd))
+		return 1;
+		if((ac==ab) && (bd==cd)) // Sometime it is observed that ab & ac , bd & cd are both same, so no need
+		return 1;
+	if((bd==ab) && (ac==cd)) // Sometime it is observed that ab & ac , bd & cd are both same, so no need
+		return 1;
+	DeltaC = (ac + bd - ab - cd);
+	if(fabs(DeltaC)<=numeric_limits<double>::epsilon())
+		return 1;
+	DeltaP = (alpha *(penalty[Tour[a]][Tour[c]]+penalty[Tour[b]][Tour[d]]-penalty[Tour[a]][Tour[b]]-penalty[Tour[c]][Tour[d]]));
+	return DeltaC+DeltaP ;
+}
+void SaveBestTour()
+{
+	bestCost = cost2;
+	BestTour.clear();
+	BestTour.insert(BestTour.begin(), Tour.begin(), Tour.end());
+}
+bool TwoOpt(int a, int b)
+{
+	//Case 1 : (prevA, A) with (b, bnext)
+	int prev_a = prev(a);
+	int next_b = next(b);
+	int next_a = next(a);
+	if(getDelta(prev_a, a, b, next_b) < 0)
+	{
+		// Found a better tour
+		cost2 += DeltaC;
+		
+		// Reverse shorter tour
+		if(getLength(a, b) < getLength(next_b, prev_a))
+			reverseTour(a, b);
+		else
+			reverseTour(next_b, prev_a);
+		// Activate the nodes
+		isActive[Tour[prev_a]] = 1;
+		isActive[Tour[a]] = 1;
+		isActive[Tour[b]] = 1;
+		isActive[Tour[next_b]] = 1;
+		if(cost2 < bestCost) SaveBestTour();
+		return 1;
+	}
+	//Case 2:  (A, Anext) with (b, bnext)
+	if(getDelta(a, next_a, b, next_b) < 0)
+	{
+		// Found a better tour
+		cost2 += DeltaC;
+		
+		// Reverse shorter tour
+		if(getLength(next_a, b) < getLength(next_b, a))
+			reverseTour(next_a, b);
+		else
+			reverseTour(next_b, a);
+		// Activate the nodes
+		isActive[Tour[a]] = 1;
+		isActive[Tour[next_a]] = 1;
+		isActive[Tour[b]] = 1;
+		isActive[Tour[next_b]] = 1;
+		if(cost2 < bestCost) SaveBestTour();
+		return 1;
+	}
+	return 0;
+}
+void FastLocalSearch()
+{
+	int visited =0;
+	int current = 0; //current node;
+	while(visited<n) // If a full loop run without improvement exit
+	{
+		LOG("BestCost is "<< bestCost<< " current "<< cost2<<" calculated " <<getCost()<<" "<<current<<" "<<visited<<endl);
+		bool modified = false;
+		if(isActive[Tour[current]])// For each active node test with (0, n-1) nodes and see if any improvement can be made.
+		{
+			int next_node = 0;
+			
+			 // For each current node , try with all 'n' nodes if move exchange result in better tour
+			while(!modified && (next_node < n))
+			{
+				if((next_node != current) && (next_node != next(current)) && (next_node != prev(current)) && (next(next_node)!=prev(current)))
+					modified = TwoOpt(current, next_node);
+				next_node ++;
+			}
+		}
+		if(modified)// we found a better move, so go a previous node and reset loop
+		{
+			visited =-1;
+			current = prev(current);
+		}
+		else
+		{
+			// since we tested this current node with all other node and still didnt find anything good
+			// mark it as inactive
+			isActive[Tour[current]] = 0;
+			current = next(current);
+		}
+		visited++;
+	}
+}
+
+void updatePenalty()
+{
+	vector<int> U;
+	float maxUtil =0.0, util;
+	int next;
+	alpha = (float)(0.3*(float)(bestCost/n));
+	for (int i=0; i< Tour.size(); i++)
+	{
+		next = ((i+1)==Tour.size()) ?0: i+1;
+		util = (adjMatrix[Tour[i]][Tour[next]] / (1.0 + penalty [Tour[i]][Tour[next]]));
+		if(util>maxUtil)
+		{
+			U.clear(); // We got a new maximum, clear out everything we saved earlier.
+			U.push_back(i);
+			maxUtil = util;
+		}
+		else if (util==maxUtil)
+			U.push_back(i);
+	}
+	
+	for (int i=0; i< U.size(); i++)
+	{
+		next = ((U[i]+1)==Tour.size()) ?0: U[i]+1;
+		penalty[Tour[U[i]]][Tour[next]]++;
+		penalty[Tour[next]][Tour[U[i]]]++;
+		//Activate neighborhood of this penalized edge.
+		isActive[Tour[U[i]]] =1;
+		isActive [Tour[next]] =1;
+	}
+}
 class DisjointSet
 {
 	private:
@@ -97,15 +297,16 @@ int main(int argc, char* argv[])
 	E <= 2*OPT
 	*/
 	ifstream cin(argv[1]);
-	int n;
 	cin >> n;
 	float points [n] [2]; // x & y co-ordinates
 	isActive = new bool[n];
+	penalty = new int*[n];
 	adjMatrix = new float* [n];
 	for (int i=0; i<n; i++)
 	{
 		isActive[i] =1;
 		adjMatrix[i] = new float[n];
+		penalty[i] =   new int [n];
 		cin >> points[i][0] >> points[i][1];
 	}
 	vector<pair<int, int> > G; // Graphs containing edges edges (u,v), edges are stored in ascending order.
@@ -113,6 +314,7 @@ int main(int argc, char* argv[])
 	for (int i=0; i<n; i++)
 	{
 		adjMatrix[i][i]=0;
+		penalty[i][i]=0;
 		for (int j=i+1; j<n; j++)
 		{
 			adjMatrix [i][j] = adjMatrix [j][i] = sqrt(pow(abs(points[i][0]-points[j][0]), 2) + pow(abs(points[i][1]-points[j][1]), 2));
@@ -120,9 +322,12 @@ int main(int argc, char* argv[])
 			G.push_back(make_pair(i, j));
 		}
 	}
+	Tour.reserve(n);
+	BestTour.reserve(n);
+#if 1
 	// Sort Graph in ascending order of edge length.
 	sort(G.begin(), G.end(), sortByEdgeLen);	
-	#ifdef DEBUG
+	#ifdef DEBUG2
 	cout <<"Adjacency Matrix ==="<<endl;
 	for (int i=0; i<n; i++,cout<<endl)
 		for (int j=0; j<n; j++)
@@ -183,7 +388,7 @@ int main(int argc, char* argv[])
 	}
 	while(!prev.empty())
 	{tour.push_back(prev.top());;prev.pop();}
-	#ifdef DEBUG
+	#ifdef DEBUG2
 	for (auto & i:tour)
 		cout << i <<" "<<endl;
 	#endif
@@ -203,7 +408,7 @@ int main(int argc, char* argv[])
 		else
 			tour[i] = -1;//delete ith item
 	
-	float cost2 =0;
+
 	int pre =-1;
 	for (auto & i:tour)
 		if(i!=-1)
@@ -214,7 +419,7 @@ int main(int argc, char* argv[])
 		}
 		
 	cost2 += adjMatrix[0][pre];	
-#ifdef DEBUG
+#ifdef DEBUG2
 	cout<<cost2<<" "<<0<<endl;
 	for (auto & i:tour)
 		if(i!=-1)
@@ -225,145 +430,29 @@ int main(int argc, char* argv[])
 	for (auto & i:tour)
 		if(i!=-1)
 			Tour.push_back(i);
-	// Tour improvement 2-opt
-	int i=0, n_visited=0;
-	while (n_visited<n)
+#else
+	for (int i=0; i<n; i++)
+	    Tour.push_back(i);
+	cost2 = 0;
+	    for(int i=0; i<Tour.size(); i++)
+	        cost2 += adjMatrix[Tour[i]][Tour[(i+1)%n]];
+#endif
+	SaveBestTour();
+	int max_no_improvement =0;
+	// Guided Local Search 
+	while(max_no_improvement < MAX_NO_IMPROVEMENT)
 	{
-		// For each point generate (prev, i) & (i, next) 
-		int prev = (i -1 +n) % n;
-		int next = (i +1 +n) % n;
-		
-		int c = (prev +2)%n;
-		int d = (prev +3)%n;
-		// Generate next pair and try each of them one by one.
-		// For example  Tour =0, 4, 1, 2, 3
-		// i =0, prev =4 , next =1, 
-		// For (4,0)...loop generates (1,2) (2,3) (3, 4) 
-		// (0,1) ... (2,3) (3,4) (4,0)
-	    // (a, b) => (prev, i)
-		//cout <<" prev: "<<prev<<endl;
-		bool not_found =true;
-		while(isActive[i] && not_found && (d!=prev))
-		{
-			//cout <<"Current: ("<<prev<<","<<i<<") Next City Pair: ("<< c<<","<<d<<")"<<endl;
-			float delta = getDelta(prev, i, c, d); // exchange (a,b) (c,d) edge and check if we can get shorter tour.
-			if(delta<0)
-			{
-				//Found a shorter tour
-				//cout << "shorter tour found"<<endl;
-				cost2 += delta;
-				//Reverse shorter tour
-				// b-c vs a-d 
-				/*
-				if b-c circuit length is smaller then "a->c-> Rev (c->b) -> b->d"
-				if a-d circuit is smaller then "Rev (a-d)-> d -> b -> c
-				*/
-				if(abs(i-c) < abs(prev-d))
-				{
-					int start = min(i, c);
-					int end = max(i, c);
-					int len  = end  - start+1;
-					for(int ii = 0; ii< len/2; ii++)
-						swap(Tour[start+ii], Tour[end-ii]);
-				}
-				else
-				{
-					int start = min(prev, d);
-					int end = max(prev, d);
-					int len  = end  - start+1;
-					for(int ii = 0; ii< len/2; ii++)
-						swap(Tour[start+ii], Tour[end-ii]);
-				}
-				not_found =false;
-				
-			}
-			else // Both replacement are longer exit
-			{
-				//cout <<"Current cost: "<<adjMatrix[prev][i]<<" "<< adjMatrix[c][d]<<endl;
-				//cout <<"Replacement: " << adjMatrix[prev][c] <<" "<< adjMatrix[i][d]<<endl;
-			}
-			c= d;
-			d = (d +1 +n) % n;
-		}
-		
-		// (a,b) => (i, next)
-		c = (i+2)%n;
-		d = (i+3)%n;
-		//cout <<" next: "<<next<<endl;
-		//not_found = true;
-		while(isActive[i] && not_found && (d!=i))
-		{
-			//cout <<"Current: ("<<i<<","<<next<<") Next City Pair: ("<< c<<","<<d<<")"<<endl;
-			float delta = getDelta(i, next, c, d); // exchange (a,b) (c,d) edge and check if we can get shorter tour.
-			if(delta<0)
-			{
-				//Found a shorter tour
-				//cout << "shorter tour found"<<endl;
-				cost2 += delta;
-				//Reverse shorter tour
-				// b-c vs a-d 
-				/*
-				if b-c circuit length is smaller then "a->c-> Rev (c->b) -> b->d"
-				if a-d circuit is smaller then "Rev (a-d)-> d -> b -> c
-				*/
-				if(abs(next-c) < abs(i-d))
-				{
-					int start = min(next, c);
-					int end = max(next, c);
-					int len  = end  - start+1;
-					for(int ii = 0; ii< len/2; ii++)
-						swap(Tour[start+ii], Tour[end-ii]);
-				}
-				else
-				{
-					int start = min(i, d);
-					int end = max(i, d);
-					int len  = end  - start+1;
-					for(int ii = 0; ii< len/2; ii++)
-						swap(Tour[start+ii], Tour[end-ii]);				}
-				not_found = false;
-			}
-			else // Both replacement are longer exit
-			{
-				//cout <<"Current cost: "<<adjMatrix[i][next]<<" "<< adjMatrix[c][d]<<endl;
-				//cout <<"Replacement: " << adjMatrix[i][c] <<" "<< adjMatrix[next][d]<<endl;
-			}
-			c= d;
-			d = (d +1 +n) % n;
-		}
-		if(not_found)
-		{
-			//cout <<"No 2-opt found for "<<i<<endl;
-			if(isActive[i])
-			{
-				n_visited++;
-				isActive[i] = 0;
-			}
-			i = (i+1)%n;
-			
-		}
+		float oldCost =bestCost;
+		//cout<<"best cost is"<< bestCost<<" "<<max_no_improvement<<endl;
+		FastLocalSearch();
+		if((oldCost-bestCost) > 1)
+		    max_no_improvement=0;
 		else
-			i = (i-1+n)%n;
-		
+		    max_no_improvement++;
+		updatePenalty();
 	}
-	cout <<cost2<<" 0"<<endl;
-	for (auto & i:Tour)
+	cout <<bestCost<<" 0"<<endl;
+	for (auto & i:BestTour)
 		cout << i<<" ";
 	cout <<endl;
-}
-// a, b, c, d  are indices in Tour array
-float getDistance (int a, int b)
-{
-	return adjMatrix[Tour[a]][Tour[b]];
-}
-// a, b, c, d  are indices in Tour array
-float getDelta(int a, int b, int c, int d)
-{
-	float ab =  getDistance(a, b);	
-	float ac =  getDistance(a, c);
-	float bd =  getDistance(b, d);
-	float cd =  getDistance(c, d);
-	if((ac > ab) && (bd > cd))
-		return 1;
-	return ac + bd - ab - cd;
 }
